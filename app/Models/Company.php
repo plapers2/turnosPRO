c1831712d9df4783ecfea06eaae3f7a41fe6d726
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-04-21 14:59:51 -0500

    Migraciones y Empresas para tipos de empresa  y empresas

---
+++ app/Models/Company.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        "name",
+        "logo",
+        "address",
+        "phone",
+        "state"
+    ];
+
+    public function type_companies()
+    {
+        return $this->belongsTo(TypeCompany::class, 'type_company_id', 'id');
+    }
+}
