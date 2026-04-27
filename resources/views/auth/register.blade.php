44057698e9892ab5e8bb53bdd5dd9be547d9845e
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-04-27 12:54:15 -0500

    Se crea register y se modifica a login funcionalidad de ver contraseña con textos a español

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,24 +1,34 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form class="flex flex-col gap-2" method="POST" action="{{ route('login') }}">
+    <form class="flex flex-col gap-2" method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Email Address -->
+        <!-- Nombre -->
         <div class="flex flex-col gap-2">
+            <x-input-label for="name" :value="__('Nombre')" />
+            <div class="relative group">
+                <span
+                    class="material-symbols-outlined absolute left-4 top-1/2 -translate-y-1/2 text-outline-variant group-focus-within:text-primary transition-colors"
+                    data-icon="person">person</span>
+                <x-text-input placeholder="Juan Pérez" id="name" class="block mt-1 w-full" type="text"
+                    name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
+        </div>
+
+        <!-- Correo -->
+        <div class="mt-4 flex flex-col gap-2">
             <x-input-label for="email" :value="__('Correo electrónico')" />
             <div class="relative group">
                 <span
                     class="material-symbols-outlined absolute left-4 top-1/2 -translate-y-1/2 text-outline-variant group-focus-within:text-primary transition-colors"
                     data-icon="mail">mail</span>
                 <x-text-input placeholder="usuario@gmail.com" id="email" class="block mt-1 w-full" type="email"
-                    name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    name="email" :value="old('email')" required autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
         </div>
 
-        <!-- Password -->
+        <!-- Contraseña -->
         <div class="mt-4 flex flex-col gap-2">
             <x-input-label for="password" :value="__('Contraseña')" />
             <div class="relative group">
@@ -26,7 +36,7 @@
                     class="material-symbols-outlined absolute left-4 top-1/2 -translate-y-1/2 text-outline-variant group-focus-within:text-primary transition-colors"
                     data-icon="lock">lock</span>
                 <x-text-input placeholder="••••••••" id="password" class="block mt-1 w-full" type="password"
-                    name="password" required autocomplete="current-password" />
+                    name="password" required autocomplete="new-password" />
                 <button
                     class="absolute right-4 top-1/2 mt-1 -translate-y-1/2 text-outline-variant hover:text-on-surface transition-colors"
                     type="button"
@@ -37,21 +47,34 @@
             </div>
         </div>
 
+        <!-- Confirmar Contraseña -->
+        <div class="mt-4 flex flex-col gap-2">
+            <x-input-label for="password_confirmation" :value="__('Confirmar contraseña')" />
+            <div class="relative group">
+                <span
+                    class="material-symbols-outlined absolute left-4 top-1/2 -translate-y-1/2 text-outline-variant group-focus-within:text-primary transition-colors"
+                    data-icon="lock">lock</span>
+                <x-text-input placeholder="••••••••" id="password_confirmation" class="block mt-1 w-full"
+                    type="password" name="password_confirmation" required autocomplete="new-password" />
+                <button
+                    class="absolute right-4 top-1/2 mt-1 -translate-y-1/2 text-outline-variant hover:text-on-surface transition-colors"
+                    type="button"
+                    onclick="togglePassword('password_confirmation', this)">
+                    <span class="material-symbols-outlined" data-icon="visibility">visibility</span>
+                </button>
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
+        </div>
+
         <div class="flex items-center justify-between px-1 my-3">
             <a class="text-body-sm text-primary font-semibold hover:underline decoration-primary/30 underline-offset-4"
-                href="{{ route('register') }}">
-                {{ __('Registrarse') }}
+                href="{{ route('login') }}">
+                {{ __('¿Ya estás registrado?') }}
             </a>
-            @if (Route::has('password.request'))
-            <a class="text-body-sm text-primary font-semibold hover:underline decoration-primary/30 underline-offset-4"
-                href="{{ route('password.request') }}">
-                {{ __('¿Olvidaste tú contraseña?') }}
-            </a>
-            @endif
         </div>
 
         <x-primary-button>
-            {{ __('Ingresar') }}
+            {{ __('Registrarse') }}
             <span class="material-symbols-outlined text-xl" data-icon="arrow_forward">arrow_forward</span>
         </x-primary-button>
 
