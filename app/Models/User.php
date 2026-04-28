c93abaeae4fc717b038dfb9dbeb43d9b53002d95
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-04-28 13:31:45 -0500

    conflicto en user.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -84,11 +84,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Appointment::class);
     }
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function services()
-    {
-        return $this->belongsToMany(Service::class);
-    }
 }
