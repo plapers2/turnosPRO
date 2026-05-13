9680a445161e5f5327dc7e9a9485283dc68321fb
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-05-13 13:13:28 -0500

    Se acepta imagen en null a la hora de crear empresa

--- database/migrations/2026_04_21_194712_create_companies_table.php
+++ database/migrations/2026_04_21_194712_create_companies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->string("logo", 255);
+            $table->string("logo", 255)->nullable();
             $table->string("email");
             $table->string("address");
             $table->string("phone");
