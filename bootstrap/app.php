81b66d4428acce2df4fcb06fe3647e9296d1026e
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-07-01 16:26:01 -0500

    Proxies para https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,12 +10,13 @@ use App\Http\Middleware\EnsurePasswordChanged;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        channels: __DIR__.'/../routes/channels.php',
+        channels: __DIR__ . '/../routes/channels.php',
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'role'              => RoleMiddleware::class,
             'permission'        => PermissionMiddleware::class,
